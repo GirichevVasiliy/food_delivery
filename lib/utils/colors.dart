e7045cf37a22d078c9805a03f30aa0fd8d97de0e
giric <girichev2013@yandex.ru>
2024-05-05 14:46:19 +0500

    05.05.24: Create main page

---
+++ lib/utils/colors.dart
@@ -0,0 +1,14 @@
+import 'dart:ui';
+
+class AppColors {
+  static Color get textColor => const Color(0xFFccc7c5);
+  static Color get mainColor => const Color(0xFF89dad0);
+  static Color get iconColor1 => const Color(0xFFffd28d);
+  static Color get iconColor2 => const Color(0xFFfcab88);
+  static Color get paraColor => const Color(0xFF8f837f);
+  static Color get buttonBackGroundColor => const Color(0xFFf7f6f4);
+  static Color get singColor => const Color(0xFFa9a29f);
+  static Color get titleColor => const Color(0xFF5c524f);
+  static Color get mainBlackColor => const Color(0xFF332d2d);
+  static Color get yellowColor => const Color(0xFFffd379);
+}
