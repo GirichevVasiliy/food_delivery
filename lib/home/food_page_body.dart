6f5a4955344734ce9a79c2af2b9d171f11c06c47
giric <girichev2013@yandex.ru>
2024-05-06 07:07:47 +0500

    06.05.24: Create main page

--- lib/home/food_page_body.dart
+++ lib/home/food_page_body.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:food_delivery/utils/colors.dart';
 import 'package:food_delivery/widgets/big_text.dart';
+import 'package:food_delivery/widgets/icon_and_text_widget.dart';
+import 'package:food_delivery/widgets/small_text.dart';
 
 // Класс карусели на главной страницы
 class FoodPageBody extends StatefulWidget {
@@ -12,11 +14,27 @@ class FoodPageBody extends StatefulWidget {
 
 class _FoodPageBodyState extends State<FoodPageBody> {
   PageController pageController = PageController(viewportFraction: 0.85);
+  var _currPageValue = 0.0; // переменная для страниц карусели, при перемотке: 1.0, 2.0 и тд номера страниц
+
+  @override
+  void initState() {
+    super.initState();
+    pageController.addListener(() {
+     setState(() {
+       _currPageValue = pageController.page!;
+     });
+    });
+  }
+
+  @override
+  void dispose(){
+    pageController.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Colors.red,
+      //color: Colors.red,
       height: 320,
       child: PageView.builder(
           controller: pageController,
@@ -33,7 +51,7 @@ class _FoodPageBodyState extends State<FoodPageBody> {
       children: [
         Container(
           height: 220,
-          margin: EdgeInsets.only(left: 5, right: 5),
+          margin: EdgeInsets.only(left: 10, right: 10),
           decoration: BoxDecoration(
               borderRadius: BorderRadius.circular(30),
               color: index.isEven ? Color(0xFF69c5df) : Color(0xFF9294cc),
@@ -44,20 +62,23 @@ class _FoodPageBodyState extends State<FoodPageBody> {
         Align(
           alignment: Alignment.bottomCenter,
           child: Container(
-            height: 140,
-            margin: EdgeInsets.only(left: 30, right: 30, bottom: 15),
+            height: 120,
+            margin: EdgeInsets.only(left: 30, right: 30, bottom: 30),
             decoration: BoxDecoration(
               borderRadius: BorderRadius.circular(30),
               color: Colors.white,
             ),
             child: Container(
-              padding: EdgeInsets.only(top: 10, left: 15, right: 15),
+              padding: EdgeInsets.only(top: 15, left: 15, right: 15),
               child: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   BigText(text: "Chinese Side"),
-                  SizedBox(height: 10,),
+                  SizedBox(
+                    height: 10,
+                  ),
                   Row(
+                    //Ряд со звездами
                     children: [
                       Wrap(
                         children: List.generate(5, (index) {
@@ -67,7 +88,38 @@ class _FoodPageBodyState extends State<FoodPageBody> {
                             size: 15,
                           );
                         }),
-                      )
+                      ),
+                      SizedBox(
+                        width: 7,
+                      ),
+                      SmallText(text: "4.5"),
+                      SizedBox(
+                        width: 20,
+                      ),
+                      SmallText(text: "1234"),
+                      SizedBox(
+                        width: 7,
+                      ),
+                      SmallText(text: "comments")
+                    ],
+                  ), //Ряд со звездами
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Row(
+                    children: [
+                      IconAndTextWidget(
+                          icon: Icons.circle_sharp,
+                          text: "Normal",
+                          iconColor: AppColors.iconColor1),
+                      IconAndTextWidget(
+                          icon: Icons.location_on,
+                          text: "1,7 km",
+                          iconColor: AppColors.mainColor),
+                      IconAndTextWidget(
+                          icon: Icons.access_time_rounded,
+                          text: "Normal",
+                          iconColor: AppColors.iconColor2),
                     ],
                   )
                 ],
